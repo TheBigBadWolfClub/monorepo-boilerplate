6495557bf2224bda9763be692a8c289b728ab911
TheBigBadWolf <TheBigBadWolfClub@gmail.com>
2022-04-03 18:02:42 +0100

    update js boilerplate

--- js/app/app.js
+++ js/app/app.js
@@ -1,12 +1,5 @@
-
-logElapsed = (startTime) => {
-    const elapsed = (new Date()).getTime() - startTime.getTime();
-    console.log('\ntotal elapsed: ' + elapsed)
-}
-
 (async () => {
-    const startTime = new Date();
+    console.time(`main`);
 
-
-    logElapsed(startTime)
+    console.timeEnd(`main`);
 })();
